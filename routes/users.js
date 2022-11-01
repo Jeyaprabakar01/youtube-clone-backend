1d2b50c543c363c96feac3d75a0f76059bf3227d
Jeyaprabakar01 <ej.prabakar@gmail.com>
2022-11-01 14:28:46 +0530

    update,delete,get user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,36 @@
 import express from "express";
-import {} from "../controllers/user.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unSubscribe,
+  update,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
+
+//update user
+router.put("/:id", verifyToken, update);
+
+//delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+//get user
+router.get("/find/:id", getUser);
+
+//subscribe a user
+router.put("/sub/:id", verifyToken, subscribe);
+
+//unsunscribe a user
+router.put("/unsub/:id", verifyToken, unSubscribe);
+
+//like a video
+router.put("/like/:videoid", verifyToken, like);
+
+//dislike a video
+router.put("/dislike/:videoid", verifyToken, dislike);
 
 export default router;
